b3a5d9e90bc877d178c7a5ad7f0ee9fa38d5c619
bug5654 <bug.new@gmail.com>
2020-02-18 19:06:30 -0800

    basic changes, modifications of tutorial

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,1 +1,7 @@
 d3.select('h1').style('color', 'red')
+    .attr('class', 'heading')
+    .text("You'll never guess what was here!")
+
+d3.select('body').append('p')
+    .text('Dynamic Paragraph')
+    .style('font-family', "monospace")
