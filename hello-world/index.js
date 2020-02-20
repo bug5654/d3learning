d2334d5471da726b4c49baff7b4478660a2bc902
bug5654 <bug.new@gmail.com>
2020-02-19 16:38:19 -0800

    copypasta from example, with some dynamism

--- hello-world/index.js
+++ hello-world/index.js
@@ -5,3 +5,16 @@ d3.select('h1').style('color', 'red')
 d3.select('body').append('p')
     .text('Dynamic Paragraph')
     .style('font-family', "monospace")
+
+var data = [10, 15, 20, 30]
+
+d3.select('body').append('div')
+    .attr('class', 'chart')
+
+d3.select(".chart")
+    .selectAll('div')
+    .data(data)
+        .enter()
+        .append("div")
+        .style("width", function(d) {return d + "px"})
+        .text(function(d) { return d; })
